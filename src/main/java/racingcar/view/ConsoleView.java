aac2293c11b5076ee1e0ac228421bf1231563bd1
millennion-jin <Jadestern2@>
2022-04-23 21:02:01 +0900

    feat: 시도 횟수 입력 기능 구현

--- src/main/java/racingcar/view/ConsoleView.java
+++ src/main/java/racingcar/view/ConsoleView.java
@@ -2,6 +2,7 @@ package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import racingcar.constants.MessageConstants;
+import racingcar.domain.AttemptsNumberInput;
 import racingcar.domain.CarNamesInput;
 
 public class ConsoleView {
@@ -10,5 +11,12 @@ public class ConsoleView {
 
         String inputValue = Console.readLine();
         return new CarNamesInput(inputValue);
+    }
+
+    public AttemptsNumberInput readAttemptsNumberInput() {
+        System.out.println(MessageConstants.CONSOLE_MESSAGE_ATTEMPTS_NUMBER);
+
+        String inputValue = Console.readLine();
+        return new AttemptsNumberInput(inputValue);
     }
 }
