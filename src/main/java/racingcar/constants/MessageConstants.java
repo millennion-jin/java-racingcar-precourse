71ee9461b418cd32b564d34cb3ab15f638d1ea5d
millennion-jin <Jadestern2@>
2022-04-23 20:41:15 +0900

    fix: 메시지 Constants에 누락된 부분 commit

--- src/main/java/racingcar/constants/MessageConstants.java
+++ src/main/java/racingcar/constants/MessageConstants.java
@@ -4,4 +4,7 @@ public class MessageConstants {
     public static final String ERROR_MESSAGE_NAME_NULL_EMPTY = "이름 값이 null 이거나 비어있습니다.";
     public static final String ERROR_MESSAGE_NAME_OVER_LIMIT = "이름은 %d 자 이하여야 합니다.";
     public static final String ERROR_MESSAGE_CAR_LIST_NULL_EMPTY = "차량 리스트가 null 이거나 비어있습니다.";
+    public static final String ERROR_MESSAGE_CAR_NAMES_INPUT_EMPTY = "차량 이름들의 입력값이 비었습니다.";
+
+    public static final String CONSOLE_MESSAGE_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 }
